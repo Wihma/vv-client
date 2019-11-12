d62998f709ec52c5824b4568cf6de4a1c9434f82
Marc Wihlstrand <marc.wihlstrand@gmail.com>
2019-11-12 20:16:29 +0100

    Wrap root Vue instance in a function that checks if users data is availabe. This to make sure no requests are made before jwt token is available.

--- vv-client/src/main.js
+++ vv-client/src/main.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -8,27 +9,26 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  vuetify,
-  created() {
-    const userString = localStorage.getItem('user')
-    if (userString) {
-      const userData = JSON.parse(userString)
-      this.$store.commit('auth/SET_USER_DATA', userData)
-      this.$router.push({ name: 'habits' })
-    }
-    axios.interceptors.response.use(
-      response => response,
-      err => {
-        if (err.response.status === 401) {
-          this.$store.dispatch('logout')
-        }
-        return Promise.reject(err)
+;(() => {
+  const userString = localStorage.getItem('user')
+  if (userString) {
+    const userData = JSON.parse(userString)
+    store.commit('auth/SET_USER_DATA', userData)
+  }
+  axios.interceptors.response.use(
+    response => response,
+    err => {
+      if (err.response.status === 401) {
+        this.$store.dispatch('logout')
       }
-    )
-  },
-  render: h => h(App)
-}).$mount('#app')
+      return Promise.reject(err)
+    }
+  )
+  new Vue({
+    router,
+    store,
+    vuetify,
+    created() {},
+    render: h => h(App)
+  }).$mount('#app')
+})()
